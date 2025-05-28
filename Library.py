6e810159f830ba23a29db94c7c897a485d5c9b42
levonchukV <levonchuk.v@kibit.edu.ua>
2025-05-28 10:54:02 +0200

    final commit

--- Library.py
+++ Library.py
@@ -1,4 +1,25 @@
 class Library:
-    def __init__(self, library_name, books):
+    def __init__(self, library_name, books=None):
         self.library_name = library_name
-        self.books = books
+        self.books = books if books is not None else []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def library_average_rating(self):
+        if not self.books:
+            return "No books in the library."
+
+        total_ratings = []
+        for book in self.books:
+            total_ratings.extend(book.ratings)
+
+        avg = sum(total_ratings) / len(total_ratings)
+        return f"Library average rating: {avg}"
+
+    def top_rated_book(self):
+        if not self.books:
+            return "No books in the library."
+
+        top_book = max(self.books, key=lambda book: sum(book.ratings) / len(book.ratings))
+        return f"Top rated book:\n{top_book}"
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from Book import Book
 from Ebook import Ebook
+from Library import Library
 
+book1 = Book("Kobzar", "Taras Shevchenko", [5, 4, 3])
+book2 = Book("Lisova pisnya", "Lesya Ukrainka", [4, 4, 5])
+ebook = Ebook("Eneida", "Ivan Kotlyarevskiy", [5, 5, 5], 1.2)
 
-book = Book("title", "author", [5, 4, 3, 5])
-print(book.average_rating())
-print(book)
-print(book.ratings_greater_than(3))
-print("\n" + "-------------------" + "\n")
+library = Library("Library1")
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(ebook)
 
-
-ebook = Ebook("E-Book", "E-Author", [4, 5, 5, 4], 2.5)
-print(ebook.average_rating())
-print(ebook)
-print(ebook.ratings_greater_than(4))
-
+print(library.library_average_rating())
+print(library.top_rated_book())
